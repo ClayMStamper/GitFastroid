599c326da8854c372fc0506cb137658ac102a7cd
Clay <claystamper@gmail.com>
2017-08-02 10:57:07 -0500

    fixed interactable stat sliders in the store, increased button control click box size, changed settings for gem sprite to attempt and correct fuzziness

---
+++ Space game/Assets/scripts/SliderFixer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderFixer : MonoBehaviour {
+
+	public Slider[] sliders;
+
+	void Start(){
+		sliders = FindObjectsOfType <Slider> ();
+		foreach (Slider slider in sliders) {
+			slider.interactable = false;
+		}
+	}
+}
