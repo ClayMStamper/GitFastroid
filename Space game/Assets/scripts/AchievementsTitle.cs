2ba7c9ccb9905e8d9138f7040a72140593b715d4
Clay <claystamper@gmail.com>
2017-09-23 12:58:01 -0500

    Achievements menu setup completely

--- Space game/Assets/scripts/AchievementsTitle.cs
+++ Space game/Assets/scripts/AchievementsTitle.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class AchievementsTitle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public Scrollbar AchievementSlider;
+	public Text AchievementTitle;
+
+	public void Start () {
+		//AchievementTitle = GetComponent <Text> ();
+		print ( AchievementSlider.value);
+		if (AchievementSlider.value <= 0) {
+			AchievementTitle.text = "Background Skins";
+		} else if (AchievementSlider.value == 0.5f) {
+			AchievementTitle.text = "Asteroid Skins";
+			print ("working");
+		} else {
+			AchievementTitle.text = "Gem Skins";
+		}
 	}
 }
